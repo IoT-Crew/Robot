15153618a535e0de797004528bcafb7f1bc6f237
HuiJeong Kang <hjkang3693@gmail.com>
2023-07-11 16:54:42 +0900

    HJ-웹페이지 실행 및 구역 버튼 클릭 시 해당 구역 정보 보냄

--- hj/src/home.js
+++ hj/src/home.js
@@ -1,25 +1,58 @@
 import React from "react";
+import axios from "axios";
 import "./home.css";
 
 function Home() {
+  const handleAreaClick = (area) => {
+    axios
+      .get(`http://192.168.0.80:1880/area?data=${area}`) // Node-RED 플로우 실행 엔드포인트와 필요한 데이터 전송
+      .then((response) => {
+        // 요청에 대한 응답 처리
+        console.log(response.data); //응답 데이터 출력
+      })
+      .catch((error) => {
+        // 오류 처리
+        console.error(error); //오류 출력
+      });
+  };
+
   return (
     <div className="Home">
       <a
-        href="http://192.168.0.101:1880/ui/#!/1?socketid=OdY7fmNICnTr9ThUAAAD"
+        href="http://192.168.0.101:1880/ui"
         target="_blank"
         rel="noopener noreferrer"
       >
         <button className="Gauge">실시간 각도 Gauge 대시보드</button>{" "}
       </a>
-      <div>
-        <button className="A">A 구역</button>
-        <button className="B">B 구역</button>
-      </div>
-      <div>
-        <button className="C">C 구역</button>
-        <button className="D">D 구역</button>
+      <div className="locationButton">
+        <div>
+          <button className="A" onClick={() => handleAreaClick("A")}>
+            A 구역
+          </button>
+          <button className="B" onClick={() => handleAreaClick("B")}>
+            B 구역
+          </button>
+        </div>
+        <div>
+          <button className="C" onClick={() => handleAreaClick("C")}>
+            C 구역
+          </button>
+          <button className="D" onClick={() => handleAreaClick("D")}>
+            D 구역
+          </button>
+        </div>
+        <div>
+          <button className="Catch" onClick={() => handleAreaClick("catch")}>
+            Catch
+          </button>
+          <button className="Put" onClick={() => handleAreaClick("put")}>
+            Put
+          </button>
+        </div>
       </div>
     </div>
   );
 }
+
 export default Home;
